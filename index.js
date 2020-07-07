3c99eaa7c6ae6c82dede0c1de07908f5cb06aa0d
Bryan D. Gonzalez <bryan.dujari@gmail.com>
2020-07-07 20:20:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function destructivelyUpdateObjectWithKeyAndValue(recipes, key, value){
 }
 
 function deleteFromObjectByKey(recipes, key){
-  var fin = Object.assign({},recipes, {[key]: value})
+
 
 }
 
